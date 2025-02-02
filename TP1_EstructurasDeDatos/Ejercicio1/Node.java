630c1cf880aa84e6ced079efcaf856b2574d56e9
Daniela Fernández <liquiddookie.17@gmail.com>
2025-02-02 14:18:02 -0300

    Updated.

---
+++ TP1_EstructurasDeDatos/Ejercicio1/Node.java
@@ -0,0 +1,32 @@
+public class Node<T> {
+    private T info;
+    private Node<T> next;
+
+    // Constructores
+    public Node() {
+        this.info = null;
+        this.next = null;
+    }
+
+    public Node(T info, Node<T> next) {
+        this.setInfo(info);
+        this.setNext(next);
+    }
+
+    // Getters y Setters
+    public T getInfo() {
+        return info;
+    }
+
+    public void setInfo(T info) {
+        this.info = info;
+    }
+
+    public Node<T> getNext() {
+        return next;
+    }
+
+    public void setNext(Node<T> next) {
+        this.next = next;
+    }
+}
